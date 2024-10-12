3d034561f277df68a5e0d03145748acd0c324230
Eugenio Guevara <guevara@MacBook-Pro-4.local>
2024-10-12 10:45:05 -0600

    fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -172,7 +172,7 @@ function NavBar() {
 
           {/* MOBILE */}
           <div className="block lg:hidden fixed inset-0 z-[999] bg-white">
-            <div className="flex justify-between items-center px-4 py-3 border-b border-gray-200">
+            <div className="flex justify-between items-center px-4 py-3 border-b border-gray-200 bg-white">
               <div className="flex items-center gap-2">
                 <button onClick={toggleMenu} className="p-2 z-10">
                   {menu ? (
