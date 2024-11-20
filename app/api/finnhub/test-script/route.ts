deb6f5b3a7e1a86bd94c6c9deeeef9111fe4125c
Eugenio Guevara <guevara@MacBook-Pro-4.local>
2024-11-20 15:08:38 -0600

    fix

--- app/api/finnhub/test-script/route.ts
+++ app/api/finnhub/test-script/route.ts
@@ -1,18 +1,19 @@
-// import { NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-// export async function GET() {
-//   const prisma = new PrismaClient();
+export async function GET() {
+  const prisma = new PrismaClient();
 
-//   try {
-//     return NextResponse.json({});
-//   } catch (error) {
-//     console.error("Error deleting transcripts:", error);
-//     return NextResponse.json(
-//       { error: "Failed to delete transcripts" },
-//       { status: 500 }
-//     );
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
+  try {
+    const companies = await prisma.company.findMany();
+    return NextResponse.json({ companies });
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch companies" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
