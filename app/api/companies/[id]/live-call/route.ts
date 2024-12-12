dd0df4b30d97ab197ff52bd06397592c433ec51d
Eugenio Guevara <guevaraeu1@gmail.com>
2024-12-12 11:53:58 -0600

    fix

--- app/api/companies/[id]/live-call/route.ts
+++ app/api/companies/[id]/live-call/route.ts
@@ -35,11 +35,15 @@ export async function GET(
       return new NextResponse("Company not found", { status: 404 });
     }
 
-    // Get today's date and format it
-    // const today = new Date();
-    // const from = today.toISOString().split("T")[0];
-    const from = "2024-01-01";
-    const to = "2024-12-30";
+    // Calculate yesterday and a week from now
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const nextWeek = new Date();
+    nextWeek.setDate(nextWeek.getDate() + 7);
+
+    const from = yesterday.toISOString().split("T")[0];
+    const to = nextWeek.toISOString().split("T")[0];
 
     const response = await fetch(
       `https://finnhub.io/api/v1/stock/earnings-call-live?` +
